86d4f9220916c99f00f74344ebadf140e2dc2f09
Alex Miller <alex.k.miller@gmail.com>
2016-02-29 15:22:50 +0000

    trying to fix db mess

--- gantcal/cal/models.py
+++ gantcal/cal/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.fields import Field
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from datetime import datetime
@@ -24,8 +25,86 @@ class MyBooleanField(models.BooleanField):
     def get_db_prep_value(self, value, connection,prepared=False):
         value = super(MyBooleanField, self).get_db_prep_value(value, connection, prepared)
         return 'true' if value else 'false'
+
+class BooleanField(Field):
+    empty_strings_allowed = False
+    default_error_messages = {
+        'invalid': ("'%(value)s' value must be either True or False."),
+    }
+    description = ("Boolean (Either True or False)")
+
+    def __init__(self, *args, **kwargs):
+        kwargs['blank'] = True
+        super(BooleanField, self).__init__(*args, **kwargs)
+
+    def check(self, **kwargs):
+        errors = super(BooleanField, self).check(**kwargs)
+        errors.extend(self._check_null(**kwargs))
+        return errors
+
+    def _check_null(self, **kwargs):
+        if getattr(self, 'null', False):
+            return [
+                checks.Error(
+                    'BooleanFields do not accept null values.',
+                    hint='Use a NullBooleanField instead.',
+                    obj=self,
+                    id='fields.E110',
+                )
+            ]
+        else:
+            return []
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(BooleanField, self).deconstruct()
+        del kwargs['blank']
+        return name, path, args, kwargs
+
+    def get_internal_type(self):
+        return "BooleanField"
+
+    def to_python(self, value):
+        if value in (True, False):
+            # if value is 1 or 0 than it's equal to True or False, but we want
+            # to return a true bool for semantic reasons.
+            return bool(value)
+        if value in ('t', 'True', 'true', '1'):
+            return True
+        if value in ('f', 'False', 'false', '0'):
+            return False
+        raise exceptions.ValidationError(
+            self.error_messages['invalid'],
+            code='invalid',
+            params={'value': value},
+        )
+
+    def get_prep_lookup(self, lookup_type, value):
+        # Special-case handling for filters coming from a Web request (e.g. the
+        # admin interface). Only works for scalar values (not lists). If you're
+        # passing in a list, you might as well make things the right type when
+        # constructing the list.
+        if value in ('1', '0'):
+            value = bool(int(value))
+        return super(BooleanField, self).get_prep_lookup(lookup_type, value)
+
+    def get_prep_value(self, value):
+        value = super(BooleanField, self).get_prep_value(value)
+        if value is None:
+            return None
+        return bool(value)
+
+    def formfield(self, **kwargs):
+        # Unlike most fields, BooleanField figures out include_blank from
+        # self.null instead of self.blank.
+        if self.choices:
+            include_blank = not (self.has_default() or 'initial' in kwargs)
+            defaults = {'choices': self.get_choices(include_blank=include_blank)}
+        else:
+            defaults = {'form_class': forms.BooleanField}
+        defaults.update(kwargs)
+        return super(BooleanField, self).formfield(**defaults)
     
-models.BooleanField = MyBooleanField
+models.BooleanField = BooleanField
 
 class Theme(models.Model):
     title = models.CharField(max_length=255,unique=True)
@@ -98,7 +177,7 @@ class Event(models.Model):
     location = models.CharField(max_length=255,null=True,blank=True)
     slug = models.SlugField(unique=True,max_length=255, null=True, blank=True,editable=False)
     start = models.DateField(auto_now=False, auto_now_add=False)
-    date_confirmed = MyBooleanField(default=True)
+    date_confirmed = models.BooleanField(default=True)
     end = models.DateField(auto_now=False, auto_now_add=False)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     modified = models.DateTimeField(auto_now=True, auto_now_add=False)
@@ -117,8 +196,8 @@ class Event(models.Model):
         ('RE','Remotely engage'),
     )
     focus = models.CharField(max_length=2,choices=FOCUS_CHOICES,default='MO')
-    objectives_approved = MyBooleanField(default=False)
-    attendees_approved = MyBooleanField(default=False)
+    objectives_approved = models.BooleanField(default=False)
+    attendees_approved = models.BooleanField(default=False)
     
     class Meta:
         ordering = ['start','title']
@@ -189,14 +268,14 @@ class Task(models.Model):
     start = models.DateField(auto_now=False, auto_now_add=False)
     duration = models.IntegerField(blank=True,null=True)
     end = models.DateField(auto_now=False, auto_now_add=False)
-    startIsMilestone = MyBooleanField(default=False)
-    endIsMilestone = MyBooleanField(default=False)
+    startIsMilestone = models.BooleanField(default=False)
+    endIsMilestone = models.BooleanField(default=False)
     assignee = models.ManyToManyField(Assignee, related_name="tasks", related_query_name="task",blank=True)
     depends = models.CharField(max_length=255,blank=True,null=True)
     description = models.TextField(null=True,blank=True)
     progress = models.IntegerField(default=0)
     event = models.ForeignKey(Event, related_name="tasks", related_query_name="task",blank=True)
-    hasChild = MyBooleanField(default=False)
+    hasChild = models.BooleanField(default=False)
     
     class Meta:
         ordering = ['start','name']
